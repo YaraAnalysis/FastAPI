57e25dfd97d7a22a9142d2e8c996ea1166e3d319
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-08-21 00:09:22 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+# Essa linha siginifica que vamos chamar nossa API de app e que esse app é uma instância do FastAPI.
+
+vendas = {
+    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
+    2: {"item": "garrafa 2L", "preco_unitario": 15, "quantidade": 5},
+    3: {"item": "garrafa 750ml", "preco_unitario": 10, "quantidade": 5},
+    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5},
+}
+
+@app.get("/")
+def home():
+    return {"Vendas": len(vendas)}
+
+@app.get("/vendas/{id_venda}")
+def pegar_venda(id_venda: int):
+    if id_venda in vendas:
+        return vendas[id_venda]
+    else:
+        return {"Erro": "ID da Venda inexistente"}
